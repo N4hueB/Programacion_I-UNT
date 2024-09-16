0e061d9e6fffd49222c103e65a60f87721c17b1c
N4hueB <nahuelbellucci2815@gmail.com>
2024-09-15 22:30:41 -0300

    Primera carga de archivos al repositorio

---
+++ 02_Desafios/Desafio_01/main.py
@@ -0,0 +1,4 @@
+from data_heroes import lista_nombres_heroes
+
+for nombre in lista_nombres_heroes:
+    print(nombre)
---
+++ 02_Desafios/Desafio_01/validaciones/validaciones.py
@@ -0,0 +1,11 @@
+def validar_opcion(n_min, n_max)->int:
+    opcion = input("ingrese una opcion: ")
+    while True:
+        if opcion.isdigit():
+            opcion = int(opcion)
+            if opcion >= n_min and opcion <= n_max:
+                return opcion
+            else:
+                opcion = input("ingrese una opcion: ")
+        else:
+                opcion = input("ingrese una opcion: ")
